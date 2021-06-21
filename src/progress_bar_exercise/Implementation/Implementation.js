5a770438b04839df925c32e0363118e583db6091
Dan Filler <dwfiller@gmail.com>
2021-06-20 22:07:12 -0400

    remove unused import

--- src/progress_bar_exercise/Implementation/Implementation.js
+++ src/progress_bar_exercise/Implementation/Implementation.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ProgressBar from './ProgressBar/ProgressBar';
 import ButtonPair from './ButtonPair/ButtonPair';
 import { REQUEST_STATES } from './constants';
