44248166812d4d4c827efaaa99b37ac307cf2370
Dan Filler <dwfiller@gmail.com>
2021-06-20 21:45:41 -0400

    progress bar animations and control with request state

--- src/progress_bar_exercise/Implementation/Implementation.js
+++ src/progress_bar_exercise/Implementation/Implementation.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProgressBar from './ProgressBar/ProgressBar';
-import Button from './ButtonPair/ButtonPair';
+import ButtonPair from './ButtonPair/ButtonPair';
+import { REQUEST_STATES } from './constants';
 
 function Solution() {
+  const [ barState, setBarState ] = useState(REQUEST_STATES.PRE);
+  useEffect(() => {
+
+  })
+
   return (
     <div>
-      <ProgressBar />
-      <Button
-        startRequest={() => console.log('start')}
-        finishRequest={() => console.log('finish')}
+      <ProgressBar barState={barState}/>
+      <ButtonPair
+        startRequest={() => setBarState(REQUEST_STATES.LOADING)}
+        finishRequest={() => {
+          setBarState(REQUEST_STATES.FINISHED);
+          setTimeout(() => {
+            setBarState(REQUEST_STATES.POST);
+          }, 3000);
+        }}
       />
     </div>
   );
--- src/progress_bar_exercise/Implementation/ProgressBar/ProgressBar.js
+++ src/progress_bar_exercise/Implementation/ProgressBar/ProgressBar.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import { REQUEST_STATES } from '../constants';
+
 import './ProgressBar.scss';
 
-function ProgressBar() {
-  const [width, setWidth] = useState(20);
+const BAR_STYLES = {
+  [REQUEST_STATES.PRE]: {
+    'width': '0%'
+  },
+  [REQUEST_STATES.LOADING]: {
+    'transitionDuration': '15s',
+    'width': '90%'
+  },
+  [REQUEST_STATES.FINISHED]: {
+    'transitionDuration': '1s',
+    'width': '100%'
+  },
+  [REQUEST_STATES.POST]: {
+    'width': '0%'
+  },
+};
 
+function ProgressBar({ barState }) {
   return (
     <div className='container'>
       <span
-        className='bar'
-        style={{width: `${width}%`}}
+        className={`bar`}
+        style={BAR_STYLES[barState]}
       />
     </div>
   );
